fe2c73eef34aac0391a6741398a26cea71e0ad77
Madisen <madisenvogel@gmail.com>
2022-11-05 22:07:36 -0600

    created seeds

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (department_name)
+VALUES ("Operations"),
+        ("Marketing"),
+        ("IT"),
+        ("HR");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Marketer", 90000, 2),
+        ("Software Engineer", 100000, 3),
+        ("Director of IT", 120000, 3),
+        ("Head of Operations", 90000, 4),
+        ("Accountant", 100000, 4),
+        ("Manufacturer", 800000, 1);
+        ("Purchaser", 800000, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Art","Vandeley", 3, NULL),
+        ("Greg","McMuffin", 2, 1),
+        ("Pepe","Sylvia", 1, 7),
+        ("Peg","Legge", 5, 7);
+        ("Saul","Goodman", 5, 7);
+        ("Scoobert","Doo", 2, 1);
+        ("Charles","Cheese", 4, NULL);
